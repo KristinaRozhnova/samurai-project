762b1b3220dab9de847de1eed9caa6a0e7d3eeae
Kristina <99440794+KristinaRozhnova@users.noreply.github.com>
2022-10-11 23:43:09 +0300

    Change App.tsx, Header.tsx, Technologies.tsx

---
+++ src/Technologies.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Technologies;
